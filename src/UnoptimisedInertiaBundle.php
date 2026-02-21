5f55a072e8a3db74c5a111727fe3a7716961a05e
unoptimised <email@unoptimised.dev>
2026-02-21 11:00:12 +0000

    fix: explicitly register InertiaExtension to bypass Symfony naming convention

--- src/UnoptimisedInertiaBundle.php
+++ src/UnoptimisedInertiaBundle.php
@@ -3,7 +3,12 @@
 namespace Unoptimised\InertiaBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Unoptimised\InertiaBundle\DependencyInjection\InertiaExtension;
 
 class UnoptimisedInertiaBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new InertiaExtension();
+    }
 }
