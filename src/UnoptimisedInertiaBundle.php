17133aa0fcc4cf330d97e0d6a66f4dc3dc26b969
Unoptimised <email@unoptimised.dev>
2026-07-07 11:25:32 +0100

    feat: add Symfony 6.4 compatibility

--- src/UnoptimisedInertiaBundle.php
+++ src/UnoptimisedInertiaBundle.php
@@ -2,12 +2,13 @@
 
 namespace Unoptimised\InertiaBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Unoptimised\InertiaBundle\DependencyInjection\InertiaExtension;
 
 class UnoptimisedInertiaBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new InertiaExtension();
     }
